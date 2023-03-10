f527f4984a401e9e7317116656439f0447438dd2
jahnavidubisetty <109265301+jahnavidubisetty@users.noreply.github.com>
2023-03-10 18:53:07 +0530

    Add files via upload

---
+++ MainMod.java
@@ -0,0 +1,8 @@
+import java.sql.SQLException;
+
+public class MainMod{
+	public static void main(String[] args) throws InterruptedException, SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
